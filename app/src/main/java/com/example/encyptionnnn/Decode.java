8cd57ba5c1d92dca75feaa919dd8fcd3fa03ba64
Jassie410 <jassie410@gmail.com>
2021-05-22 14:11:33 +0530

    first commit

---
+++ app/src/main/java/com/example/encyptionnnn/Decode.java
@@ -0,0 +1,27 @@
+package com.example.encyptionnnn;
+
+public class Decode {
+    public static String dec(String s){
+        if ((s.length() % 8 != 3) || (s.indexOf("101") != 0)) {
+            return "ERROR : This code was not encrypted by SMS Encrypter !!!";
+        }
+
+        String data = s.substring(3, s.length() - 7); //removing prefix and suffix as per the algo
+        int[] binary = new int[] { 128, 64, 32, 16, 8, 4, 2, 1 };
+
+        String res = "";
+        int sum=0;
+        for (int i = 0; i < data.length(); i++) {
+
+            int bindex = i % 8;
+            if (i % 8 == 0) {
+                res = res + (char) sum;
+                sum = 0;
+            }
+            if (data.charAt(i) == '1') {
+                sum = sum + binary[bindex];
+            }
+        }
+        return res;
+    }
+}
---
+++ app/src/main/java/com/example/encyptionnnn/Encode.java
@@ -0,0 +1,27 @@
+package com.example.encyptionnnn;
+
+public class Encode {
+    String toBinary(int num) {
+        int[] bin = new int[8];
+        int i = 7;
+        while (num > 0) {
+            bin[i] = num % 2;
+            num = num / 2;
+            i--;
+        }
+        String binaryString="";
+        for (i = 0; i < 8; i++) {
+            binaryString = binaryString + bin[i];
+        }
+        return binaryString;
+    }
+
+    String enc(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            int ascii = (int) s.charAt(i);
+            res = res + toBinary(ascii);
+        }
+        return "101" + res + toBinary(s.length()); //prefix + original data binary + suffix
+    }
+}
